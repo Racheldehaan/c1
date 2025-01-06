a8f21eebb2e5f307289c098dab7686604e3786c9
Davy <90331454+Davytjuhh@users.noreply.github.com>
2025-01-06 13:20:49 +0100

    Back-end update

--- app.py
+++ app.py
@@ -2,11 +2,26 @@ from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), 'templates'))
-app.config['DEBUG'] = True 
+app.config['DEBUG'] = True
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def home():
     return render_template('base.html')
+
+@app.route('/output', methods=['POST'])
+def output():
+    text_input = request.form.get('text-input')
+    suggestions = []
+    
+    # Hier komt de logica voor het maken van suggesties op basis van de input
+    if text_input:
+        suggestions = [f"• {x}" for x in text_input]
+        language_level = "B2"
+    
+    else:
+        language_level = None
+    
+    return render_template('base.html', suggestions=suggestions, language_level=language_level)
 
 if __name__ == "__main__":
     app.run(debug=True)
